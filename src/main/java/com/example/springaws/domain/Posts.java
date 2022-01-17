daab6cccd9a984d9d6e9c47a32c499dd9727de63
blackarea <supersute142@gmail.com>
2022-01-17 16:01:32 +0900

    post 수정

--- src/main/java/com/example/springaws/domain/Posts.java
+++ src/main/java/com/example/springaws/domain/Posts.java
@@ -4,16 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Posts extends BaseTimeEntity{
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 500, nullable = false)
